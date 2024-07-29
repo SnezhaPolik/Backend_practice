57b8e23a913cb7a364de74a0be15cf95582eb54a
Snezha <anna.nasta@mail.ru>
2024-07-29 06:34:08 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,58 +1,17 @@
 import './style.css';
+import React from 'react'
 
-const questions = [
-  {
-    title: 'JavaScript- это ... ?',
-    variants: ['язык программирования', 'учебник', 'приложение'],
-    correct: 0,
-  },
-  {
-    title: 'Backend-разработчик - это человек, который создает ... ',
-    variants: ['дизайн', 'серверную часть приложения или страницы', 'то, что я не знаю что такое'],
-    correct: 1,
-  },
-  {
-    title: 'Тебе понравилась профессия backend-разработчик?',
-    variants: [
-      'Да',
-      'Нет',
-      'Не знаю, не уверен(а)',
-    ],
-    correct: 2,
-  },
-];
-
-function Result() {
-  return (
-    <div className="result">
-      <img src="https://www.flaticon.com/free-icon/paw_8277564" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
-    </div>
-  );
-}
-
-function Game() {
-  return (
-    <>
-      <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
-      </div>
-      <h1>Что такое useState?</h1>
-      <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
-      </ul>
-    </>
-  );
-}
 
 function App() {
+
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      <div>
+        <h2>Счетчик</h2>
+        <h1>0</h1>
+        <button  className="minus">- Минус</button>
+        <button  className="plus">Плюс +</button>
+      </div>
     </div>
   );
 }
